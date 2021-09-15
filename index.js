18f6bca95479e2bf7611536295c7bdef9f2b6eb5
Andrew Plummer <plummer.andrew@gmail.com>
2021-09-15 19:24:35 +0900

    added imports plugin

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = {
   configs: {
     recommended: require('./recommended.json'),
+    imports: require('./imports.json'),
     react: require('./react.json'),
     jest: require('./jest.json'),
-  }
-}
+  },
+};
