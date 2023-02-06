1e5e1264ad7c73279da1c81b897fde06572a671e
Andrew Plummer <plummer.andrew@gmail.com>
2023-02-06 23:20:18 +0900

    added imports-node

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   configs: {
     recommended: require('./recommended.json'),
+    'imports-node': require('./imports-node.json'),
     'imports-webpack': require('./imports-webpack.json'),
     'imports-typescript': require('./imports-typescript.json'),
     react: require('./react.json'),
